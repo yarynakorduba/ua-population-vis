e37eb755a50c9eb455020a469b05b4bab2629846
yarynakorduba <korduba_y@ucu.edu.ua>
2018-12-07 23:55:25 +0200

    Updates

---
+++ precache-manifest.de6fdb8dbae4e229cf53a7fb266687c8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dfa798ddd4d9023ec07b",
+    "url": "/yarynakorduba/yarynakorduba.github.io/static/js/runtime~main.dfa798dd.js"
+  },
+  {
+    "revision": "b163646330aee1d82133",
+    "url": "/yarynakorduba/yarynakorduba.github.io/static/js/main.b1636463.chunk.js"
+  },
+  {
+    "revision": "1d1c40af9bfa457bae87",
+    "url": "/yarynakorduba/yarynakorduba.github.io/static/js/1.1d1c40af.chunk.js"
+  },
+  {
+    "revision": "b163646330aee1d82133",
+    "url": "/yarynakorduba/yarynakorduba.github.io/static/css/main.34880b1e.chunk.css"
+  },
+  {
+    "revision": "db2891f37607ccbd7878fbf4364d6e52",
+    "url": "/yarynakorduba/yarynakorduba.github.io/index.html"
+  }
+];
