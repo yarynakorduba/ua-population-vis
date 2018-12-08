475e1e2941bddb188ceecfca1ace41b9b4c71508
mykhailoivankiv <neformal.lviv@gmail.com>
2018-12-08 07:16:33 +0200

    Updates

---
+++ precache-manifest.a8027b600d82fcec6cd011b3c7e27f1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "33b34a305bef82e96ba8",
+    "url": "/ua-population-vis/static/js/runtime~main.33b34a30.js"
+  },
+  {
+    "revision": "7f8dc4cb371bc5116146",
+    "url": "/ua-population-vis/static/js/main.7f8dc4cb.chunk.js"
+  },
+  {
+    "revision": "e3192de76435c1c1b7ba",
+    "url": "/ua-population-vis/static/js/1.e3192de7.chunk.js"
+  },
+  {
+    "revision": "7f8dc4cb371bc5116146",
+    "url": "/ua-population-vis/static/css/main.159ea27e.chunk.css"
+  },
+  {
+    "revision": "7be146d295f64fc61aef51d517172ecf",
+    "url": "/ua-population-vis/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/ua-population-vis/precache-manifest.211890f994d8801c983af688df3b1010.js"
+  "/ua-population-vis/precache-manifest.a8027b600d82fcec6cd011b3c7e27f1e.js"
 );
 
 workbox.clientsClaim();
